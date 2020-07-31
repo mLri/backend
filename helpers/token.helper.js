e4e18478b33fc6719be49efedc47e3a16152a6cc
Tosaporn Maphasuk <tosaporn@homedottech.com>
2020-07-31 12:37:53 +0700

    Done basic auth project

---
+++ helpers/token.helper.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+module.exports.checkAuth = (req, res, next) => {
+  const authToken = req.headers['authorization']
+  if(!authToken) return res.sendStatus(403)
+  const token = authToken.split(' ')[1]
+
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+    req.user = verified
+    next()
+  } catch (err) {
+    console.log(err)
+    res.status(403).json(err)
+  }
+}
+
+module.exports.createAccessToken = (payload) => {
+  return jwt.sign({ ...payload }, process.env.TOKEN_SECRET, { expiresIn: '1d' })
+}
+
+module.exports.createRefreshToken = (payload) => {
+  return jwt.sign({ ...payload }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
+}
--- validation/schema/auth.validate.js
+++ validation/schema/auth.validate.js
@@ -5,12 +5,15 @@ module.exports = {
     signup: Joi.object({
       first_name: Joi.string().min(3).max(30).required(),
       last_name: Joi.string().min(3).max(30).required(),
-      password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/),
-      username: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+      password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).required(),
+      username: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()
     }),
     signin: Joi.object({
-      password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/),
-      username: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+      password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).required(),
+      username: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()
+    }),
+    refresh_token: Joi.object({
+      refresh_token: Joi.string().required()
     })
   }
 }
