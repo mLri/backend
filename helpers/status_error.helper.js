0f5bcc0fb463d35e1b156b707d1981ce664c6f60
Tosaporn Maphasuk <tosaporn@homedottech.com>
2020-07-31 15:07:40 +0700

    use handleError in checkAuth func

--- helpers/status_error.helper.js
+++ helpers/status_error.helper.js
@@ -9,6 +9,14 @@ module.exports = {
       message: msg
     }
   },
+  unauthorized: {
+    status_error: 401,
+    message: "Unauthorized"
+  },
+  forbidden: {
+    status_code: 403,
+    message: "Forbidden You don't have permission to access the specified resource",
+  },
   not_found: {
     status_error: 404,
     message: 'The requested resource could not be found but may be available in the future. Subsequent requests by the client are permissible.'
--- helpers/token.helper.js
+++ helpers/token.helper.js
@@ -1,17 +1,26 @@
 const jwt = require('jsonwebtoken')
 
-module.exports.checkAuth = (req, res, next) => {
-  const authToken = req.headers['authorization']
-  if(!authToken) return res.sendStatus(403)
-  const token = authToken.split(' ')[1]
+/* include helpers */
+const { handleError } = require('../helpers/handle_error.helper')
+const statusError = require('../helpers/status_error.helper')
 
+module.exports.checkAuth = (req, res, next) => {
   try {
+
+    const auth_token = req.headers['authorization']
+    if (!auth_token) throw statusError.unauthorized
+
+    const split_auth_token = auth_token.split(' ')
+    const bearer = split_auth_token[0]
+    const token = split_auth_token[1]
+
+    if (!bearer || bearer !== 'Bearer') throw statusError.forbidden
+
     const verified = jwt.verify(token, process.env.TOKEN_SECRET)
     req.user = verified
     next()
-  } catch (err) {
-    console.log(err)
-    res.status(403).json(err)
+  } catch (error) {
+    handleError(error, res)
   }
 }
 
