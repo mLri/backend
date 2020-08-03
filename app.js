5a86295ce822eecef4633dd524e6a24c24610928
Tosaporn Maphasuk <tosaporn@homedottech.com>
2020-08-03 10:46:31 +0700

    connect mongo atlas

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(cors({
 }))
 
 /* mongodb connection */
-const mongoURI = `mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// const mongoURI = `mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+const mongoURI = `mongodb://mlrigod:${process.env.DB_PASSWORD}@cluster0-shard-00-00.gs0hy.mongodb.net:27017,cluster0-shard-00-01.gs0hy.mongodb.net:27017,cluster0-shard-00-02.gs0hy.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-26its2-shard-0&authSource=admin&retryWrites=true&w=majority`
 
 mongoose
   .connect(mongoURI, {
